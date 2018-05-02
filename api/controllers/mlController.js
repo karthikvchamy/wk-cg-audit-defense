3c1aa8406e8e15cf0d535f9b3b6cd443c1547a43
Stuart, Sathya <Sathya.Stuart@wolterskluwer.com>
2018-05-02 15:21:19 -0500

    path issue fixed

--- api/controllers/mlController.js
+++ api/controllers/mlController.js
@@ -3,23 +3,27 @@ const brain = require("brain.js");
 const network = new brain.NeuralNetwork();
 const db = require("../../db.json");
 const fs = require('fs');
+var path = require('path');
+
 exports.train = function (req, res) {
     
     res.json({ "test": "hello" });
     //console.log(req.body);
-    fs.readFile('../../Codegames2018/wk-cg-audit-defense/return_mocks/input/' + req.body.id + '.json', 'utf8', function (err, data) {
+    console.log(__dirname);
+    var jsonPath = path.join(__dirname, '../..', 'return_mocks', 'input', req.body.id + '.json');
+    fs.readFile(jsonPath, 'utf8', function (err, data) {
         if (err) {
             return console.log(err);
         }
         
-
-        fs.readFile('../../Codegames2018/wk-cg-audit-defense/db.json', 'utf8', function (e, d) {
+var dbPath = path.join(__dirname, '../..', 'db.json');
+        fs.readFile(dbPath, 'utf8', function (e, d) {
             
             var result = JSON.parse(d)
             //d.returns.push({ "data": data, "id": 4 });
             result.returns.push(JSON.parse(data));
             //console.log(result);
-            fs.writeFile('../../Codegames2018/wk-cg-audit-defense/db.json', JSON.stringify(result), function (err1) {
+            fs.writeFile(dbPath, JSON.stringify(result), function (err1) {
                 if (err1) {
                     return console.log(err1);
                 }
@@ -35,8 +39,9 @@ exports.assess = function (req, res) {
     const data = db.returns.map((d) => { return d.data });
 
     network.train(data);
-
-    fs.readFile('../../Codegames2018/wk-cg-audit-defense/return_mocks/output/' + req.body.id + '.json', 'utf8', function (ex, dt) {
+    var outputPath = path.join(__dirname, '../..', 'return_mocks', 'output', req.body.id + '.json');
+    console.log(outputPath);
+    fs.readFile(outputPath, 'utf8', function (ex, dt) {
         var rs = JSON.parse(dt);        
 
         const result = network.run(rs.data);
--- server.js
+++ server.js
@@ -6,14 +6,20 @@ var express = require('express'),
 
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
-
   var routes = require('./api/routes/mlRoutes'); //importing route
   routes(app); //register the route
 
   app.listen(port);
 
-  app.use(function (req, res) {
-      res.status(404).send({ url: req.originalUrl + ' not found' })
+  app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
   });
+
+
 
   console.log('todo list RESTful API server started on: ' + port);
